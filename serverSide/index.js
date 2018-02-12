3d7c542ee0df2afe5450b555c3bbb4677ebfd192
anton202 <a.kluge202@gmail.com>
2018-02-12 12:41:57 +0200

    fixed refresh

--- serverSide/index.js
+++ serverSide/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const generalRoutes = require('./Routes/generalRoutes.js')
 const userRoutes = require('./Routes/userRoutes.js')
+const path = require('path');
 
 app.use(express.static('./clientSide/kickStarter/dist'))
 app.use('/api/general',generalRoutes);
 app.use('/api/user',userRoutes);
+app.get('*', (req, res) => {
+    return res.sendFile(path.join('/home/anton/dev/kickStarter/serverSide/clientSide/kickStarter/dist', '/index.html'));
+});
 
 app.listen(8080, () => console.log('server runing'));
