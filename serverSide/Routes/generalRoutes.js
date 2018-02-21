2573b6b727a47c280582e91383bc8179fdfba527
anton202 <a.kluge202@gmail.com>
2018-02-21 18:23:25 +0200

    rewrited userArea end point

--- serverSide/Routes/generalRoutes.js
+++ serverSide/Routes/generalRoutes.js
@@ -7,57 +7,77 @@ const logic = require('../logic.js');
 const Sequelize = require('sequelize');
 
 
-router.get('/viewAll/:id',(req,res)=>{
-const category = req.params.id;
+router.get('/viewAll/:id', (req, res) => {
+  const category = req.params.id;
 
-projects.findAll({where:{category},include:[{model:contributedMoney}]})
- .then(projects => {
-    projects = projects.map(project => project.toJSON());
+  projects.findAll({
+      where: {
+        category
+      },
+      include: [{
+        model: contributedMoney
+      }]
+    })
+    .then(projects => {
+      projects = projects.map(project => project.toJSON());
 
-    projects.forEach(project =>{
+      projects.forEach(project => {
         project.totalAmountContributed = logic.totalAmountContributed(project);
         project.backers = logic.totalbackers(project);
       })
       res.json(projects);
-  })
+    })
 })
 
 
-router.get('/viewProject/:id',(req,res)=>{
+router.get('/viewProject/:id', (req, res) => {
   const projectId = req.params.id;
 
-  projects.findOne({where:{id:projectId},include:[{model:contributedMoney}]})
-  .then((project)=>{
-    project = project.toJSON();
+  projects.findOne({
+      where: {
+        id: projectId
+      },
+      include: [{
+        model: contributedMoney
+      }]
+    })
+    .then((project) => {
+      project = project.toJSON();
 
-    project.totalAmountContributed = logic.totalAmountContributed(project);
-    project.backers = logic.totalbackers(project);
+      project.totalAmountContributed = logic.totalAmountContributed(project);
+      project.backers = logic.totalbackers(project);
 
-    res.json(project);
-  });
+      res.json(project);
+    });
 
 })
 
 
-router.get('/preview/:id',(req,res)=>{
-   const category = req.params.id;
-   projects.findAll({where:{category},
-     include:[{model:contributedMoney}],
-     order:[ Sequelize.fn( 'RAND' )],
-     limit:3})
-     .then(projects =>{
-       projects = projects.map(project => project.toJSON());
+router.get('/preview/:id', (req, res) => {
+  const category = req.params.id;
+  projects.findAll({
+      where: {
+        category
+      },
+      include: [{
+        model: contributedMoney
+      }],
+      order: [Sequelize.fn('RAND')],
+      limit: 3
+    })
+    .then(projects => {
+      projects = projects.map(project => project.toJSON());
 
-       projects.forEach(project =>{
-         project.totalAmountContributed = logic.totalAmountContributed(project);
-         project.backers = logic.totalbackers(project);
-        })
-       res.json(projects);
-     })
+      projects.forEach(project => {
+        project.totalAmountContributed = logic.totalAmountContributed(project);
+        project.backers = logic.totalbackers(project);
+      })
+      res.json(projects);
+    })
 })
 
 
-router.get('/stats',async (req,res)=>{
+router.get('/stats', async (req, res) => {
   const stats = {};
   stats.totalUsers = await users.count();
   stats.totalProjects = await projects.count();
--- serverSide/Routes/userRoutes.js
+++ serverSide/Routes/userRoutes.js
@@ -14,8 +14,10 @@ router.post('/register',(req,res)=>{
   const password = req.body.data.password;
   const email = req.body.data.email;
 
-    users.findAll({where: {email}}).then(data=>{
-    if(data.length !== 0) return res.json("email already exist")
+    users.findAll({where: {email}})
+    .then(user=>{
+    if(user.length !== 0)
+    return res.json("email already exist")
 
     bcrypt.genSalt(saltRounds, (err, salt) => {
     bcrypt.hash(password, salt, (err, hashedPassword) => {
@@ -38,19 +40,22 @@ router.post('/login',(req,res)=>{
   const email = req.body.data.email;
   const password = req.body.data.password;
 
-    users.findAll({where:{email}}).then(async data=>{
-    let info = await data.map((d)=>d.toJSON());
+    users.findOne({where:{email}})
+    .then( user =>{
+      user = user.toJSON();
+console.log(user,password)
+    if(user.length === 0)
+    return res.json("email dose not exist in dataBase");
 
-    if(info.length === 0) return res.json("email dose not exist in dataBase");
-    bcrypt.compare(password, info[0].password, (err, isValid) => {
+    bcrypt.compare(password, user.password, (err, isValid) => {
       if (isValid === true) {
-        req.session.userId = info[0].id;
-        req.session.user = info[0];
+        req.session.userId = user.id;
+        req.session.user = user;
 
         return res.json({"message":"secsesfuly loged in",
                         "status":true,
-                        "userName":info[0].name,
-                        "userId":info[0].id
+                        "userName":user.name,
+                        "userId":user.id
                         })
       };
       res.json({"message":"wrong password",
@@ -72,11 +77,13 @@ router.get('/logOut',(req,res)=>{
 })
 
 router.get('/userArea',(req,res)=>{
-  projects.findAll({where:{userId:req.session.userId}}).then(data=>{
-    let info = data.map(d=>d.toJSON());
-    console.log(info);
-    res.json(info)});
+  projects.findAll({where:{userId:req.session.userId}}).then(projects=>{
+  res.json(projects)
 })
+})
+/*  let info = data.map(d=>d.toJSON());
+  console.log(info);
+  res.json(info)});*/
 
 
 router.post('/startProject',(req,res)=>{
